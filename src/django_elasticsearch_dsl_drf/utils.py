c14d7f26834c7a090688db441498b65613ade536
Whanderley Souza Freitas <whanderley.souza@iff.edu.br>
2024-07-09 11:29:27 -0300

    fix to use newst elasticsearch-dsl

--- src/django_elasticsearch_dsl_drf/utils.py
+++ src/django_elasticsearch_dsl_drf/utils.py
@@ -3,7 +3,7 @@ Utils.
 """
 
 import datetime
-from elasticsearch_dsl.search import AggsProxy
+from elasticsearch_dsl.search_base import AggsProxy
 
 
 __title__ = 'django_elasticsearch_dsl_drf.utils'
